6c5c07779d3c2d0dc5f69da6176b65d9871c122d
Musaraf-M <mohammedmusaraf.mm@psnacet.edu.in>
2021-01-16 12:29:00 +0530

    final

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,77 +1,59 @@
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
 var config = require('../config/config');
-
-function createToken (user) {
+ 
+function createToken(user) {
     return jwt.sign({ id: user.id, email: user.email }, config.jwtSecret, {
-        expiresIn:86400
-    });
+        expiresIn: 86400 // 86400 expires in 24 hours
+      });
 }
-
-exports. registerUser = (req,res) => {
-    if(!req.body.email || !req.body.password) {
-        return res.status(400).json({
-            'msg': 'You need to send email and password'
-        });
+ 
+exports.registerUser = (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).json({ 'msg': 'You need to send email and password' });
     }
-    User.findOne({
-        email: req.body.email
-    }, (err, user) => {
-        if(err) {
-            return res.status(400).json({
-                'msg': err
-            });
+ 
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (err) {
+            return res.status(400).json({ 'msg': err });
         }
-        if(user) {
-            return res.status(400).json({
-                'msg': 'The user already exists'
-            });
+ 
+        if (user) {
+            return res.status(400).json({ 'msg': 'The user already exists' });
         }
-
+ 
         let newUser = User(req.body);
         newUser.save((err, user) => {
-            if(err) {
-                return res.status(400).json ({
-                    'msg': err
-                });
+            if (err) {
+                return res.status(400).json({ 'msg': err });
             }
-            return res.status(200).json(user);
+            return res.status(201).json(user);
         });
-    })
+    });
 };
-
-exports.loginUser = (req,res) => {
-    if(!req.body.email || !req.body.password) {
-        return res.status(400).json({
-            'msg': 'You need to send email and password'
-        });
+ 
+exports.loginUser = (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        return res.status(400).send({ 'msg': 'You need to send email and password' });
     }
-    User.findOne({
-        email: req.body.email
-    }, (err, user) => {
-        if(err) {
-            return res.status(400).json({
-                'msg': err
-            });
+ 
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (err) {
+            return res.status(400).send({ 'msg': err });
         }
-        if(!user) {
-            return res.status(400).json({
-                'msg': 'The user does not exists'
-            });
+ 
+        if (!user) {
+            return res.status(400).json({ 'msg': 'The user does not exist' });
         }
-
+ 
         user.comparePassword(req.body.password, (err, isMatch) => {
-            if(isMatch && !err) {
+            if (isMatch && !err) {
                 return res.status(200).json({
                     token: createToken(user)
                 });
+            } else {
+                return res.status(400).json({ msg: 'The email and password don\'t match.' });
             }
-            else {
-                return res.status(400).json({
-                    'msg': 'The email and password don\'t match.'
-                })
-            }
-        })
-    })
+        });
+    });
 };
-
--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -1,22 +1,21 @@
- var User = require('../models/user');
- var JwtStrategy = require('passport-jwt').Strategy,
-    ExtractJwt = require('passport-jwt').ExtractJwt;
-var config = require('../config/config');
-
+var User        = require('../models/user');
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt  = require('passport-jwt').ExtractJwt;
+var config      = require('../config/config');
+ 
 var opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: config.jwtSecret
 }
-
-module.exports = new JwtStrategy(opts, function(jwt_payload, done){
-    User.findById(jwt_payload.id, function (err, user){
-        if(err){
+ 
+module.exports = new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findById(jwt_payload.id, function (err, user) {
+        if (err) {
             return done(err, false);
         }
-        if(user) {
+        if (user) {
             return done(null, user);
-        }
-        else {
+        } else {
             return done(null, false);
         }
     });
